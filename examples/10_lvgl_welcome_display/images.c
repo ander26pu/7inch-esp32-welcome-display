4ba67bbb7cf0e17a8084ecddc0181248fbeb6ff5
Anthony Porras <anthony.porras.u@gmail.com>
2026-01-20 15:45:48 -0500

    se generaron los archivos para la prueba

---
+++ examples/10_lvgl_welcome_display/images.c
@@ -0,0 +1,6 @@
+#include "images.h"
+
+const ext_img_desc_t images[2] = {
+    { "Bienvenido", &img_bienvenido },
+    { "Adelante", &img_adelante },
+};
---
+++ examples/10_lvgl_welcome_display/screens.c
@@ -0,0 +1,84 @@
+#include <string.h>
+
+#include "screens.h"
+#include "images.h"
+#include "fonts.h"
+#include "actions.h"
+#include "vars.h"
+#include "styles.h"
+#include "ui.h"
+
+#include <string.h>
+
+objects_t objects;
+lv_obj_t *tick_value_change_obj;
+uint32_t active_theme_index = 0;
+
+void create_screen_main() {
+    lv_obj_t *obj = lv_obj_create(0);
+    objects.main = obj;
+    lv_obj_set_pos(obj, 0, 0);
+    lv_obj_set_size(obj, 800, 480);
+    {
+        lv_obj_t *parent_obj = obj;
+        {
+            // Bienvenido_img
+            lv_obj_t *obj = lv_img_create(parent_obj);
+            objects.bienvenido_img = obj;
+            lv_obj_set_pos(obj, 0, 0);
+            lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+            lv_img_set_src(obj, &img_bienvenido);
+        }
+    }
+    
+    tick_screen_main();
+}
+
+void tick_screen_main() {
+}
+
+void create_screen_adelante() {
+    lv_obj_t *obj = lv_obj_create(0);
+    objects.adelante = obj;
+    lv_obj_set_pos(obj, 0, 0);
+    lv_obj_set_size(obj, 800, 480);
+    {
+        lv_obj_t *parent_obj = obj;
+        {
+            // Adelante_img
+            lv_obj_t *obj = lv_img_create(parent_obj);
+            objects.adelante_img = obj;
+            lv_obj_set_pos(obj, 0, 0);
+            lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+            lv_img_set_src(obj, &img_adelante);
+        }
+    }
+    
+    tick_screen_adelante();
+}
+
+void tick_screen_adelante() {
+}
+
+
+
+typedef void (*tick_screen_func_t)();
+tick_screen_func_t tick_screen_funcs[] = {
+    tick_screen_main,
+    tick_screen_adelante,
+};
+void tick_screen(int screen_index) {
+    tick_screen_funcs[screen_index]();
+}
+void tick_screen_by_id(enum ScreensEnum screenId) {
+    tick_screen_funcs[screenId - 1]();
+}
+
+void create_screens() {
+    lv_disp_t *dispp = lv_disp_get_default();
+    lv_theme_t *theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), true, LV_FONT_DEFAULT);
+    lv_disp_set_theme(dispp, theme);
+    
+    create_screen_main();
+    create_screen_adelante();
+}
