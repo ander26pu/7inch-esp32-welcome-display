fd065c25f34274d92b96e71388ec6bc3089502f3
Anthony Porras <anthony.porras.u@gmail.com>
2026-01-20 15:44:00 -0500

    Se agregó un programa de prueba para validar EEZ studio con LVGL

---
+++ examples/10_lvgl_welcome_display/10_lvgl_welcome_display.ino
@@ -0,0 +1,65 @@
+#include <Arduino.h>
+#include <esp_display_panel.hpp>
+
+#include <lvgl.h>
+#include "lvgl_v8_port.h"
+//#include <demos/lv_demos.h>
+
+#include "ui.h"
+#include "actions.h"
+
+using namespace esp_panel::drivers;
+using namespace esp_panel::board;
+
+/**
+ * To use the built-in examples and demos of LVGL uncomment the includes below respectively.
+ */
+ // #include <demos/lv_demos.h>
+ // #include <examples/lv_examples.h>
+
+void setup()
+{
+    String title = "LVGL porting example";
+
+    Serial.begin(115200);
+
+    Serial.println("Initializing board");
+    Board *board = new Board();
+    board->init();
+
+    #if LVGL_PORT_AVOID_TEARING_MODE
+    auto lcd = board->getLCD();
+    // When avoid tearing function is enabled, the frame buffer number should be set in the board driver
+    lcd->configFrameBufferNumber(LVGL_PORT_DISP_BUFFER_NUM);
+#if ESP_PANEL_DRIVERS_BUS_ENABLE_RGB && CONFIG_IDF_TARGET_ESP32S3
+    auto lcd_bus = lcd->getBus();
+    /**
+     * As the anti-tearing feature typically consumes more PSRAM bandwidth, for the ESP32-S3, we need to utilize the
+     * "bounce buffer" functionality to enhance the RGB data bandwidth.
+     * This feature will consume `bounce_buffer_size * bytes_per_pixel * 2` of SRAM memory.
+     */
+    if (lcd_bus->getBasicAttributes().type == ESP_PANEL_BUS_TYPE_RGB) {
+        static_cast<BusRGB *>(lcd_bus)->configRGB_BounceBufferSize(lcd->getFrameWidth() * 10);
+    }
+#endif
+#endif
+    assert(board->begin());
+
+    Serial.println("Initializing LVGL");
+    lvgl_port_init(board->getLCD(), board->getTouch());
+
+    Serial.println("Creating UI");
+    /* Lock the mutex due to the LVGL APIs are not thread-safe */
+    lvgl_port_lock(-1);
+
+    ui_init();
+
+    /* Release the mutex */
+    lvgl_port_unlock();
+}
+
+void loop()
+{
+    //Serial.println("IDLE loop");
+    //delay(1000);
+}
